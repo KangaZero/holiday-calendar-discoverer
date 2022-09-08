3adabfae1360f5294e9b8d9dec4e9ce812215ec0
Akhilesh Sharma <vidhi.sydney07@gmail.com>
2022-09-08 10:54:44 +1000

    changed javascript file for fetching and matching date with input date

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var dateInputE1 =$('#datepicker');
 var formE1 = $('#Day-form')
 var m = moment();
-
+var arr1= [];
+var dateString ='';
+var countryCodes= ['AU','','','','','','']
 // Datepicker widget
 $(function () {
     $('#datepicker').datepicker({
@@ -21,17 +23,50 @@ $(function () {
     console.log(dateInput);
   //  var myDate= moment(str, 'YYYY-MM-DD').toDate();
 
-  var arr1 = dateInput.split('/');
- 
+   arr1 = dateInput.split('/');
+   
+
+   dateString = arr1[2]+'-'+ arr1[0]+ '-'+ arr1[1];
+   console.log(dateString.length);
   
   console.log('Month: ', arr1[0]);
   console.log('date: ', arr1[1]);
   console.log('year: ', arr1[2]);
+  console.log('Date entered :: '+typeof dateString);
+  
 
-//getHoidayInfo()
+  getHoidayInfo()
 
  }
 
  formE1.on('submit',handleSubmit);
+
+function getHoidayInfo () { 
+  
+  var requestUrl = 'https://date.nager.at/api/v3/publicholidays/'+ arr1[2] + '/AU';
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(data){
+   // console.log(data[1].name);
+   
+     for (var i = 0; i < data.length; i++) {
+      console.log( data[i].date.length);
+      console.log(dateString.length);
+      if(( data[i].date === dateString )){
+        console.log(data[i].name);
+        var showHoliday = document.querySelector('#Australia');
+        showHoliday.textContent=data[i].name;
+      }
+      else{
+        console.log("No holiday Found");
+      }
+    }
+         
+  });
+ 
+}
 
 
