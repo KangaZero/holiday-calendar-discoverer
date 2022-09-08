b0a8a1203028505442638b62f750ade612b4c312
Samuel <samuelyongw@gmail.com>
2022-09-08 20:13:29 +1000

    New api, with Title, Info, and Url rendered onto wikipage.html

--- wikipage/wiki.js
+++ wikipage/wiki.js
@@ -1,56 +1,37 @@
-
 var imgEl = document.querySelector('#img');
 var titleEl = document.querySelector('#holiday-title');
 var infoEl = document.querySelector('#info');
 var wikiLinkEl = document.querySelector('#wiki');
+var homeBtnEl = document.querySelector('#return-to-home');
 
 
 function getApi(requestUrl){
-//currently testing Australia Day
+    //add index.html holiday tag id variable here  //christmas = placeholder for now
+    var holiday = "christmas"
+    holiday = capitalizeFirstLetter(holiday)
+
 //need to include query parameter 'origin=*' to work
-var holiday = "Australia Day"
-holiday = capitalizeFirstLetter(holiday)
-var requestUrl = "https://en.wikipedia.org/w/api.php?origin=*&action=parse&format=json&page=" + holiday
+    var requestUrl = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch=" + holiday
 
 fetch(requestUrl)
 .then(function (response){
     if (response.ok){
         response.json().then(function (data){
-            //test
-            console.log(data);
 
-            //gets the title
-            var apiTitle = data.parse.title
+        //initial debugging 
+            // console.log(data);
+            // console.log(data.query.search[0].title)
+            // console.log(data.query.search[0].snippet)
 
-            //still testing
-            //gets main info
-            var apiText = data.parse.text["*"]
-           
-            // We want apiText$( "p:nth-child(2/3)" )  //currently do not know how to do
-            //var apiUrl = data.parse.iwlinks[0].url
-            var apiUrl = "https://en.wiktionary.org/wiki/" + holiday
-            
-            //gets random img name 
-            //TODO: Get image url 
-            var i = Math.floor(Math.random() * data.parse.images.length)
-            var apiImg = data.parse.images[i]
-            
-                    console.log("apiUrl", apiUrl)
-                    console.log("apiText", apiText)
+          var apiTitle = data.query.search[0].title
+          var apiText = data.query.search[0].snippet
+          var apiUrl = "https://en.wiktionary.org/wiki/" + holiday
+      
+        //renders from top to bottom of page
+        renderTitle(apiTitle)
+        renderInfo(apiText)
+        renderUrlLink(apiUrl)
 
-            function renderTitle(){
-                titleEl.innerHTML = apiTitle
-            }
-            function renderInfo(){
-                infoEl.innerHTML = apiText
-            }
-            function renderUrlLink(){
-                wikiLinkEl.setAttribute("href", apiUrl)
-            }
-            //renders from top to bottom of page
-            renderTitle()
-            renderInfo()
-            renderUrlLink()
         });
     } else {
         alert ("Error: " + response.statusText + "\n  Page not found");
@@ -60,12 +41,26 @@ fetch(requestUrl)
 }
 getApi();
 
-//no img url given in API 
-function renderImage(){
-    imgEl.setAttribute('src',)
-}
-
 //https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
+
+
+//render functions 
+  function renderTitle(apiTitle){
+    titleEl.innerHTML = apiTitle
+ }
+
+ function renderInfo(apiText){
+    infoEl.innerHTML = apiText
+}
+
+function renderUrlLink(apiUrl){
+    wikiLinkEl.setAttribute("href", apiUrl)
+}
+
+homeBtnEl.addEventListener('click', function(){
+    //not tested yet
+    window.location.assign('./index.html')
+})
